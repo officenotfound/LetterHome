99f93e7613a4902e456f0c25a74b3e667d0b27ad
officenotfound <404officenotfound@gmail.com>
2026-05-14 18:49:01 -0400

    Add branded 404 page

--- server.js
+++ server.js
@@ -1683,5 +1683,9 @@ function buildOccasionReminderEmail(occ, lastOrder) {
   };
 }
 
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Letterhome running on port ${PORT}`));
